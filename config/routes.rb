b7f962b5b142b1f2f667e7fb7a75ffb40e881b54
Eduardo Koike <edu.fox@gmail.com>
2020-06-14 10:29:41 -0300

    Add route to user Registration

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   post 'risk_evaluations', to: 'risk_evaluations#create'
   post 'authenticate', to: 'authentication#authenticate'
+  post 'sign_up', to: 'registrations#create'
 end
